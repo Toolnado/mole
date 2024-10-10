66b1482cb4db926966ec11cb1eec0c99683d611c
Toolnado <toolnado@gmail.com>
2024-10-10 23:48:07 +0300

    refactor(cmd): refactor tryout main

--- cmd/main.go
+++ cmd/main.go
@@ -1,123 +1,39 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"log"
-	"net"
+	"mole/internal/mole"
 	"os"
 	"path/filepath"
+	"sync"
 )
 
 func main() {
-	littleMole1 := NewMole("localhost:8081")
-	littleMole2 := NewMole("localhost:8082")
-
-	go littleMole2.Listen()
-
 	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
 	if err != nil {
 		log.Fatal(err)
 	}
-	filename := filepath.Join(dir, "from", "test.txt")
-	littleMole1.SendFile("localhost:8082", filename)
 
-	select {}
-}
+	moles := []*mole.Mole{}
+	for i := 0; i < 6; i++ {
+		moles = append(moles, mole.NewMole(fmt.Sprintf("localhost:808%d", i+1)))
+	}
+	go moles[0].Listen()
+	wg := sync.WaitGroup{}
+	for i, lm := range moles[1:] {
+		wg.Add(1)
+		littleMole := lm
+		filename := filepath.Join(dir, "from", fmt.Sprintf("test_%d.txt", i+1))
+		go func() {
+			if err := littleMole.SendFile("localhost:8081", filename); err != nil {
+				log.Println("littleMole: ", err)
+			}
+			wg.Done()
+		}()
+	}
 
-type Mole struct {
-	Address string
-}
-
-func NewMole(address string) *Mole {
-	return &Mole{Address: address}
-}
-
-func (m *Mole) Listen() {
-	protoError := errors.New("mole.Listen error")
-	listener, err := net.Listen("tcp", m.Address)
-	if err != nil {
-		log.Println(errors.Join(protoError, err))
-		return
-	}
-	for {
-		conn, err := listener.Accept()
-		if err != nil {
-			log.Println(errors.Join(protoError, err))
-			continue
-		}
-		go m.handleConn(conn)
-	}
-}
-
-func (m *Mole) handleConn(conn net.Conn) {
-	protoError := errors.New("mole.handleConn error")
-	defer conn.Close()
-	buf := make([]byte, 1024)
-	n, err := conn.Read(buf)
-	if err != nil {
-		log.Println(errors.Join(protoError, err))
-	}
-	filename := string(buf[:n])
-	fmt.Println(filename)
-	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
-	if err != nil {
-		log.Println(errors.Join(protoError, err))
-	}
-	file, err := os.Create(filepath.Join(dir, "to", filename))
-	if err != nil {
-		log.Println(errors.Join(protoError, err))
-	}
-	defer file.Close()
-	n, err = conn.Read(buf)
-	if err != nil {
-		log.Println(errors.Join(protoError, err))
-		return
-	}
-	_, err = file.Write([]byte("new "))
-	if err != nil {
-		log.Println(errors.Join(protoError, err))
-		return
-	}
-	_, err = file.Write(buf[:n])
-	if err != nil {
-		log.Println(errors.Join(protoError, err))
-		return
-	}
-	log.Printf("file %s received successfully.\n", filename)
-}
-
-func (p *Mole) SendFile(address string, filepath string) {
-	protoError := errors.New("mole.SendFile error")
-	conn, err := net.Dial("tcp", address)
-	if err != nil {
-		log.Println(errors.Join(protoError, err))
-		return
-	}
-	defer conn.Close()
-	filename, err := os.Stat(filepath)
-	if err != nil {
-		log.Println(errors.Join(protoError, err))
-		return
-	}
-	conn.Write([]byte(filename.Name()))
-	file, err := os.Open(filepath)
-	if err != nil {
-		log.Println("Error opening file:", err)
-		return
-	}
-	defer file.Close()
-
-	buf := make([]byte, 1024)
-	n, err := file.Read(buf)
-	if err != nil {
-		log.Println("Error reading file:", err)
-		return
-	}
-	_, err = conn.Write(buf[:n])
-	if err != nil {
-		log.Println(errors.Join(protoError, err))
-		return
-	}
-	log.Printf("File %s sent to %s.\n", filepath, address)
+	wg.Wait()
+	moles[0].Wait()
+	moles[0].Stop()
 }
